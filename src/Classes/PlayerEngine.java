883e1202cff55a9ea313accfbd0873a61ee8e158
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-03-08 13:42:29 +0100

    Created metods in PlayerEngine: start() createMedia()
    Picture creation implemented and tested

--- src/Classes/PlayerEngine.java
+++ src/Classes/PlayerEngine.java
@@ -1,4 +1,98 @@
 package Classes;
 
+import java.util.Scanner;
+
 public class PlayerEngine {
+
+    private Media[] fileStorage;
+
+    public PlayerEngine(){
+        Media[] files = new Media[5];
+        this.fileStorage = files;
+    }
+
+    private void createMedia(Scanner scanner) {
+        int action = -1; //
+        while (action < 0 || action > 2) {
+            System.out.println("Adding new media, please choose file type");
+            System.out.println("1: Picture");
+            System.out.println("2: Track");
+            System.out.println("0: Movie");
+            System.out.print("Please type a number between 0 to 2 then press ENTER: ");
+
+            if (scanner.hasNextInt()) {
+                action = scanner.nextInt();
+                scanner.nextLine();
+                switch (action) {
+                    case 1:
+                        System.out.println("Please insert picture description.");
+                        String picTitle = scanner.nextLine();
+                        Picture newPic = new Picture(picTitle);
+                        newPic.run();
+                        break;
+                    case 2:
+                        System.out.println("Please insert track title.");
+                        String trackTitle = scanner.nextLine();
+                        System.out.println("Please insert track duration.");
+                        int duration = -1;
+                        while (duration < 0) {
+                            if (scanner.hasNextInt()) {
+                                duration = scanner.nextInt();
+                                scanner.nextLine();
+
+                                if (duration < 0) {
+                                    System.out.println("Please insert a positive number for the duration.");
+                                }
+                            } else {
+                                System.out.println("Invalid input. Please insert an integer for the duration.");
+                                scanner.next();
+                            }
+                        }
+                        Audio newtrack = new Audio(trackTitle, duration);
+                        newtrack.run();
+                        break;
+                    case 0:
+                        System.out.println("Please insert movie title.");
+                        String movieTitle = scanner.nextLine();
+                        Picture newMobie = new Picture(movieTitle);
+                        newMobie.run();
+                        break;
+                    default:
+                        System.out.println("Invalid command. Please type a number between 0 to 2.");
+                        action = -1; // Ritorna nel loop
+                        break;
+                }
+            } else {
+                System.out.println("Invalid command. Please type a number between 0 to 2.");
+                scanner.next(); // Consuma caratteri residui per pulire il prossimo scan
+            }
+        }
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Chose an option:");
+            System.out.println("1: Add a new media");
+            System.out.println("2: Browse");
+            System.out.println("0: Exit");
+            System.out.print("Type an option between 0 and 2 then press ENTER: ");
+            int action = scanner.nextInt();
+
+            switch (action) {
+                case 1:
+                    createMedia(scanner);
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    System.out.println("Shutting down..");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid input, try again.");
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import Classes.Audio;
 import Classes.Movie;
 import Classes.Picture;
+import Classes.PlayerEngine;
 
 import java.util.Scanner;
 
@@ -8,35 +9,9 @@ public class Main {
     public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 
-System.out.println("Applicazione avviata.");
-
-        Picture fotoPazu = new Picture("Pazuzu che guarda dalla finestra");
-        Picture fotoLili = new Picture("Lilith che dorme");
-
-        fotoPazu.run();
-        fotoLili.show();
-
-        fotoPazu.increaseBrightness(10);
-        fotoLili.decreaseBrightness(10);
-
-        fotoPazu.run();
-        fotoLili.run();
-
-        fotoLili.increaseBrightness(3);
-        fotoPazu.decreaseBrightness(4);
-
-        fotoPazu.run();
-        fotoLili.run();
-
-        System.out.println("-------------------------------------------------------------------------------------------");
-
-        Movie pazuzuVideo = new Movie("Pazuzu che dorme", 10);
-
-        pazuzuVideo.play();
-
-        Audio rammSong = new Audio("Dicke Titten - Rammstein", 22);
-
-        rammSong.play();
+    System.out.println("Application started");
+    PlayerEngine player = new PlayerEngine();
+    player.start();
 
     }
 }
